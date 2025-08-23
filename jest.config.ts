d64cfc6e22913cb800e06fbfb9b2e160def54d50
Iskhak Abdusalamov <hello.iskhak@gmail.com>
2025-08-23 15:51:17 +0500

    feat: add Prisma and PostgreSQL setup

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   clearMocks: true,
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
 
